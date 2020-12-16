23082947c63eb2ee245abdd1082a94bc925b754c
Eric <70232980+pogman96@users.noreply.github.com>
2020-12-16 02:49:32 -0500

    added the ability to select viewing graphs with different x-axis's

--- visualGraph.py
+++ visualGraph.py
@@ -2,6 +2,13 @@ import pandas
 import plotly.express as px
 
 
+# def showGraph(stateName):
+
+# lol = pandas.read_csv("https://raw.githubusercontent.com/pogman96/covidschooldata/main/parsed/cali.csv")
+# fig = px.line(lol, x='Date', y='Case increase (per 100,000)', title='Data plotting of California Coronavirus Cases')
+# fig.show()
+
+
 def askContinuation():
     while True:
         shouldContinue = input("Would you like to see another graph?(y/n): ")
@@ -14,19 +21,47 @@ def askContinuation():
             continue
 
 
-statesDict = {"california": ["https://raw.githubusercontent.com/pogman96/school-paper-data-analysis/main/7day-average/cali7day.csv",
-                            "https://raw.githubusercontent.com/pogman96/school-paper-data-analysis/main/parsed/cali.csv"],
-              "florida": ["https://raw.githubusercontent.com/pogman96/school-paper-data-analysis/main/7day-average/florida7day.csv",
-                         "https://raw.githubusercontent.com/pogman96/school-paper-data-analysis/main/parsed/florida.csv"],
-              "new york": ["https://raw.githubusercontent.com/pogman96/school-paper-data-analysis/main/7day-average/ny7day.csv",
-                          "https://raw.githubusercontent.com/pogman96/school-paper-data-analysis/main/parsed/ny.csv"],
-              "oklahoma": ["https://raw.githubusercontent.com/pogman96/school-paper-data-analysis/main/7day-average/ok7day.csv",
-                          "https://raw.githubusercontent.com/pogman96/school-paper-data-analysis/main/parsed/oklahoma.csv"],
-              "texas": ["https://raw.githubusercontent.com/pogman96/school-paper-data-analysis/main/7day-average/texas7day.csv",
-                       "https://raw.githubusercontent.com/pogman96/school-paper-data-analysis/main/parsed/texas.csv"]}
+def determineDictionary():
+    while True:
+        view = int(input("What x-axis would you like?\n1. Dates\n2. Numbers\n"))
+        if float(view).is_integer() and 1 <= view <= 2:
+            if view == 1:
+                return {"california": [
+                    "https://raw.githubusercontent.com/pogman96/school-paper-data-analysis/main/7day-average/cali7day.csv",
+                    "https://raw.githubusercontent.com/pogman96/school-paper-data-analysis/main/parsed/cali.csv"],
+                        "florida": [
+                            "https://raw.githubusercontent.com/pogman96/school-paper-data-analysis/main/7day-average/florida7day.csv",
+                            "https://raw.githubusercontent.com/pogman96/school-paper-data-analysis/main/parsed/florida.csv"],
+                        "new york": [
+                            "https://raw.githubusercontent.com/pogman96/school-paper-data-analysis/main/7day-average/ny7day.csv",
+                            "https://raw.githubusercontent.com/pogman96/school-paper-data-analysis/main/parsed/ny.csv"],
+                        "oklahoma": [
+                            "https://raw.githubusercontent.com/pogman96/school-paper-data-analysis/main/7day-average/ok7day.csv",
+                            "https://raw.githubusercontent.com/pogman96/school-paper-data-analysis/main/parsed/oklahoma.csv"],
+                        "texas": [
+                            "https://raw.githubusercontent.com/pogman96/school-paper-data-analysis/main/7day-average/texas7day.csv",
+                            "https://raw.githubusercontent.com/pogman96/school-paper-data-analysis/main/parsed/texas.csv"]}
+            else:
+                return {"california": [
+                    "https://raw.githubusercontent.com/pogman96/school-paper-data-analysis/main/7day-average/dates%20replaced%20with%20numbers/cali7day.csv",
+                    "https://raw.githubusercontent.com/pogman96/school-paper-data-analysis/main/parsed/dates%20replaced%20with%20numbers/cali.csv"],
+                        "florida": [
+                            "https://raw.githubusercontent.com/pogman96/school-paper-data-analysis/main/7day-average/dates%20replaced%20with%20numbers/florida7day.csv",
+                            "https://raw.githubusercontent.com/pogman96/school-paper-data-analysis/main/parsed/dates%20replaced%20with%20numbers/florida.csv"],
+                        "new york": [
+                            "https://raw.githubusercontent.com/pogman96/school-paper-data-analysis/main/7day-average/dates%20replaced%20with%20numbers/ny7day.csv",
+                            "https://raw.githubusercontent.com/pogman96/school-paper-data-analysis/main/parsed/dates%20replaced%20with%20numbers/ny.csv"],
+                        "oklahoma": [
+                            "https://raw.githubusercontent.com/pogman96/school-paper-data-analysis/main/7day-average/dates%20replaced%20with%20numbers/ok7day.csv",
+                            "https://raw.githubusercontent.com/pogman96/school-paper-data-analysis/main/parsed/dates%20replaced%20with%20numbers/oklahoma.csv"],
+                        "texas": [
+                            "https://raw.githubusercontent.com/pogman96/school-paper-data-analysis/main/7day-average/dates%20replaced%20with%20numbers/texas7day.csv",
+                            "https://raw.githubusercontent.com/pogman96/school-paper-data-analysis/main/parsed/dates%20replaced%20with%20numbers/texas.csv"]}
+
 
 satisfied = False
 while not satisfied:
+    statesDict = determineDictionary()
     uInput = input("State?: ").lower()
 
     if uInput in statesDict:
@@ -35,7 +70,7 @@ while not satisfied:
         while graphChoice != 1 or graphChoice != 2:
 
             if float(graphChoice).is_integer() and 1 <= graphChoice <= 2:
-                data = pandas.read_csv(statesDict[uInput][int(graphChoice)-1])
+                data = pandas.read_csv(statesDict[uInput][int(graphChoice) - 1])
 
                 if graphChoice == 2:
                     yAxis = "Case increase (per 100,000)"
